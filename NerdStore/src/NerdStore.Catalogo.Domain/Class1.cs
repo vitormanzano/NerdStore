4a65f6848cfafddb9e32b55d1054585c95e28470
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-21 20:50:46 -0300

    feat: Creating product and category

--- NerdStore/src/NerdStore.Catalogo.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NerdStore.Catalogo.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ NerdStore/src/NerdStore.Core/DomainObjects/IAggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public interface IAggregateRoot //Mark interface, the class that implement this interface it's a aggregate root
+    {
+    }
+}
